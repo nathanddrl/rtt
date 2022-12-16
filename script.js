36d015b38c0347195c8340412bdf1a0f1f224845
Nathan <nathan.dudreuil@laposte.net>
2022-12-16 13:31:44 +0100

    fix

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ socketIo.on("connection", (clientSocket) => {
 });
 
 // Configurer la route d'accueil pour servir le fichier HTML
-app.use(express.static("./index.html"));
+app.use(express.static("./"));
 
 // Écouter les connexions sur le port 3000
 httpServer.listen(process.env.PORT || 8080);
